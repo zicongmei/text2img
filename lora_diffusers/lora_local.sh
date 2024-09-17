1cb9dae61b950b9a3eceeb34df8923b5413ad7cb
zicongmei <zicongmei@gmail.com>
2024-09-17 10:45:02 -0700

    add lora

---
+++ lora_diffusers/lora_local.sh
@@ -0,0 +1,30 @@
+
+#/bin/bash
+
+set -ex
+
+git clone https://github.com/shirayu/example_lora_training.git || echo "git done"
+
+LABEL="andy" 
+
+accelerate launch \
+    --num_processes 1 \
+    --num_machines 1 \
+    --mixed_precision "fp16" \
+    --dynamo_backend "no" \
+    train_dreambooth_lora.py \
+    --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1" \
+    --instance_data_dir="example_lora_training/diffusers_lora_example/img_train_512x512" \
+    --output_dir="models" \
+    --instance_prompt="${LABEL}" \
+    --resolution=512 \
+    --train_batch_size=1 \
+    --sample_batch_size=1 \
+    --gradient_accumulation_steps=1 \
+    --gradient_checkpointing \
+    --learning_rate=1e-4 \
+    --lr_scheduler="constant" \
+    --lr_warmup_steps=0 \
+    --checkpointing_steps=100 \
+    --mixed_precision=fp16 \
+    --max_train_steps=1000
