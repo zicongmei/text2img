8aa20ca3762388027d4c54893844e603f40ada63
zicongmei <zicongmei@gmail.com>
2024-09-20 14:13:45 -0700

    split flow

--- stable-diffusion-3-medium.py
+++ stable-diffusion-3-medium.py
@@ -1,13 +1,13 @@
-from pathlib import Path
+
 import text2img
 
 model_id = "stabilityai/stable-diffusion-3-medium-diffusers"
 
-access_token = Path("/cert/hg_token").read_text().strip()
+
 
 
 def run():
-    text2img.run(model_id=model_id, token=access_token)
+    text2img.run(model_id=model_id)
 
 
 if __name__ == "__main__":
--- text2img.py
+++ text2img.py
@@ -2,28 +2,40 @@ import torch
 import time
 from accelerate import Accelerator
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler, StableDiffusion3Pipeline
-
+from pathlib import Path
 
 class Text2Img:
-    def __init__(self, model_id, access_token=None):
+    def __init__(self, model_id):
+        self.access_token = Path("/cert/hg_token").read_text().strip()
         self.model_id = model_id
         self.model_name = model_id.split('/')[1]
-        
+
+        if "stable-diffusion-3" in self.model_name:
+            self.stable_diffussion3()
+        else:
+            self.stable_diffussion()
+
+
+    def stable_diffussion(self):
+        self.pipe = StableDiffusionPipeline.from_pretrained(
+            self.model_id,
+            torch_dtype=torch.float16,
+            token=self.access_token)
+        self.pipe.scheduler = DPMSolverMultistepScheduler.from_config(
+            self.pipe.scheduler.config)
+        self.pipe = self.pipe.to("cuda")
+
+    def stable_diffussion3(self, model_id):
         num_gpus = torch.cuda.device_count()
         print(f"Number of GPUs available: {num_gpus}")
 
         # Initialize the accelerator
         accelerator = Accelerator()
-        try:
-            self.pipe = StableDiffusionPipeline.from_pretrained(
+
+        self.pipe = StableDiffusion3Pipeline.from_pretrained(
                 self.model_id,
                 torch_dtype=torch.float16,
-                token=access_token)
-        except:
-            self.pipe = StableDiffusion3Pipeline.from_pretrained(
-                self.model_id,
-                torch_dtype=torch.float16,
-                token=access_token,
+                token=self.access_token,
                 device_map="balanced",
                 )
 
@@ -48,14 +60,14 @@ class Text2Img:
         return image
 
 
-def run_once(model_id, token=None):
-    s = Text2Img(model_id, token)
+def run_once(model_id):
+    s = Text2Img(model_id)
     prompt = input(s.model_name+"> ")
     return s.generate(prompt)
 
 
-def run(model_id, token=None):
-    s = Text2Img(model_id, token)
+def run(model_id):
+    s = Text2Img(model_id)
 
     while True:
         prompt = input(s.model_name+"> ")
